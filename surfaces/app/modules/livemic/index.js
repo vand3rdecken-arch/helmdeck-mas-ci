a20908fc7a9fbe873b5fedee3145bfc723a003cd
Tien Duy Vo <vo_duy_tien@yahoo.de>
2026-08-27 11:05:21 +0200

    Revert "Henry baseline - snapshot before hands-on judgement turn"
    This reverts commit 58d556ebd0ad0faffcc02f4ee4d0e148ddc03318.

--- surfaces/app/modules/livemic/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const expo_modules_core_1 = require("expo-modules-core");
-exports.default = (0, expo_modules_core_1.requireOptionalNativeModule)("LiveMic");
